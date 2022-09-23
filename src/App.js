4de8b3598f14a19a8bba578f4d548e5c35fe3ff6
akhil-bp <akhilbp94@gmail.com>
2022-09-23 22:53:25 +0530

    bootstarp issue and json updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useRoutes } from "react-router-dom";
 import Themeroutes from "./routes/Router";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const App = () => {
   const routing = useRoutes(Themeroutes);
